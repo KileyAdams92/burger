5e30d90afaa6cee9bb1906670f4c11c0990ed0a8
Kiley Adams <Kileyadams92@gmail.com>
2018-04-25 16:11:29 -0500

    creating more files

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Cheese Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Swiss Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Steak Burger", true);
