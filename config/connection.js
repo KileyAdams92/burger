27ab6cbf0c07cc3de7cd70783a5322108a3deb66
Kiley Adams <Kileyadams92@gmail.com>
2018-04-25 18:15:27 -0500

    continued development of logic

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
-  password: "",
-  database: "burger_db"
+  password: "ComputerNerd1",
+  database: "burgers_db"
 });
 
 connection.connect(function(err) {
